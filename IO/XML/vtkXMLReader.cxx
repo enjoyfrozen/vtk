5fc9e1fe4c73cf01a9a8da1962067b107ce89039
Sean McBride <sean@rogue-research.com>
2019-01-13 16:59:01 -0500

    A few code changes to compile as C++14 and C++17
    Still C++11 compatible of course.

--- IO/XML/vtkXMLReader.cxx
+++ IO/XML/vtkXMLReader.cxx
@@ -996,17 +996,17 @@ namespace {
 
 void ltrim(std::string &s)
 {
-  s.erase(s.begin(),
-          std::find_if(s.begin(),
-                       s.end(),
-                       std::not1(std::ptr_fun<int, int>(isspace))));
+    s.erase(s.begin(),
+            std::find_if(s.begin(),
+                         s.end(),
+                         [] (int ch) { return !std::isspace(ch); }));
 }
 
 void rtrim(std::string &s)
 {
   s.erase(std::find_if(s.rbegin(),
                        s.rend(),
-                       std::not1(std::ptr_fun<int, int>(isspace))).base(),
+                       [] (int ch) { return !std::isspace(ch); }).base(),
           s.end());
 }
 
