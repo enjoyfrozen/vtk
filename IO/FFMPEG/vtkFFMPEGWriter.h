8e349c31351cfbf6462c5782530d7c0b1929a9b3
Kairong Jiang <kairong.jiang@kitware.com>
2019-05-17 15:00:13 -0400

    added h264 encoding and mp4 output to FFMPEGWriter

--- IO/FFMPEG/vtkFFMPEGWriter.h
+++ IO/FFMPEG/vtkFFMPEGWriter.h
@@ -37,12 +37,60 @@
 
 class vtkFFMPEGWriterInternal;
 
+//enum class OutputFormats
+//{
+//  OUTPUT_FORMAT_AVI,
+//  OUTPUT_FORMAT_MP4
+//};
+//VTK_GENERATE_BITFLAG_OPS(OutputFormats);
+//
+//std::ostream &operator<<(std::ostream &os, const OutputFormats &format)
+//{
+//  switch (format)
+//  {
+//    case OutputFormats::OUTPUT_FORMAT_AVI:
+//      os << "avi";
+//      return os;
+//    case OutputFormats::OUTPUT_FORMAT_MP4:
+//      os << "mp4";
+//      return os;
+//    default:
+//      os << "Unsupported format.";
+//      return os;
+//  }
+//}
+//
+//enum class EncodingMethods
+//{
+//  ENCODING_METHOD_H264,
+//  ENCODING_METHOD_MJPEG
+//};
+//VTK_GENERATE_BITFLAG_OPS(EncodingMethods);
+//
+//std::ostream &operator<<(std::ostream &os, const EncodingMethods &method)
+//{
+//  switch (method)
+//  {
+//    case EncodingMethods::ENCODING_METHOD_H264:
+//      os << "h264";
+//      return os;
+//    case EncodingMethods::ENCODING_METHOD_MJPEG:
+//      os << "mjpeg";
+//      return os;
+//    default:
+//      os << "Unsupported encoding method.";
+//      return os;
+//  }
+//}
+
 class VTKIOFFMPEG_EXPORT vtkFFMPEGWriter : public vtkGenericMovieWriter
 {
 public:
   static vtkFFMPEGWriter *New();
-  vtkTypeMacro(vtkFFMPEGWriter,vtkGenericMovieWriter);
-  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+vtkTypeMacro(vtkFFMPEGWriter, vtkGenericMovieWriter);
+
+  void PrintSelf(ostream &os, vtkIndent indent) override;
 
   //@{
   /**
@@ -50,7 +98,9 @@ public:
    * and then end the writing process.
    */
   void Start() override;
+
   void Write() override;
+
   void End() override;
   //@}
 
@@ -61,6 +111,7 @@ public:
    * 2 means best quality and largest file size
    */
   vtkSetClampMacro(Quality, int, 0, 2);
+
   vtkGetMacro(Quality, int);
   //@}
 
@@ -70,7 +121,9 @@ public:
    * Turning off compression overrides quality setting.
    */
   vtkSetMacro(Compression, bool);
+
   vtkGetMacro(Compression, bool);
+
   vtkBooleanMacro(Compression, bool);
   //@}
 
@@ -78,7 +131,8 @@ public:
   /**
    * Set/Get the frame rate, in frame/s.
    */
-  vtkSetClampMacro(Rate, int , 1, 5000);
+  vtkSetClampMacro(Rate, int, 1, 5000);
+
   vtkGetMacro(Rate, int);
   //@}
 
@@ -87,6 +141,7 @@ public:
    * Set/Get the bit-rate
    */
   vtkSetMacro(BitRate, int);
+
   vtkGetMacro(BitRate, int);
   //@}
 
@@ -95,11 +150,40 @@ public:
    * Set/Get the bit-rate tolerance
    */
   vtkSetMacro(BitRateTolerance, int);
+
   vtkGetMacro(BitRateTolerance, int);
+  //@}
+
+  //@{
+  /**
+   * Set/Get the encoding format. e.g h264
+   */
+  vtkSetMacro(EncodingMethod, std::string);
+
+  vtkGetMacro(EncodingMethod, std::string);
+  //@}
+
+  //@{
+  /**
+   * Set/Get the name of the encoder. e.g. libopenh264
+   */
+  vtkSetMacro(CodecName, std::string);
+
+  vtkGetMacro(CodecName, std::string);
+  //@}
+
+  //@{
+  /**
+   * Set/Get the output file format. e.g. mp4
+   */
+  vtkSetMacro(OutputFormat, std::string);
+
+  vtkGetMacro(OutputFormat, std::string);
   //@}
 
 protected:
   vtkFFMPEGWriter();
+
   ~vtkFFMPEGWriter();
 
   vtkFFMPEGWriterInternal *Internals;
@@ -110,10 +194,14 @@ protected:
   int BitRate;
   int BitRateTolerance;
   bool Compression;
+  std::string EncodingMethod;
+  std::string CodecName;
+  std::string OutputFormat;
 
 private:
-  vtkFFMPEGWriter(const vtkFFMPEGWriter&) = delete;
-  void operator=(const vtkFFMPEGWriter&) = delete;
+  vtkFFMPEGWriter(const vtkFFMPEGWriter &) = delete;
+
+  void operator=(const vtkFFMPEGWriter &) = delete;
 };
 
 #endif
