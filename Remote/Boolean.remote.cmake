1d3f5c6d9a9255ed79b2a6e0458a49f3e3c308f1
Bill Lorensen <bill.lorensen@gmail.com>
2019-08-19 14:54:05 -0700

    ENH: Add Roemer's Boolean Operation code as a Remote Module.

---
+++ Remote/Boolean.remote.cmake
@@ -0,0 +1,10 @@
+#
+# A new boolean operations filter for VTK by Ronald Romer
+#
+
+vtk_fetch_module(Boolean
+  "A new boolean operation filter by Ronald Romer"
+  GIT_REPOSITORY https://github.com/lorensen/vtkbool
+  # 19 August 2019
+  GIT_TAG 2b231d2371e6453199ae9e88b2504fd0661595ce
+  )
