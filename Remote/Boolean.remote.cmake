d6b00e4c3c53245ec2f543ee4d0d02a54017633e
Bill Lorensen <bill.lorensen@gmail.com>
2019-08-26 16:04:56 -0400

    Fix HeaderTest errors

--- Remote/Boolean.remote.cmake
+++ Remote/Boolean.remote.cmake
@@ -6,5 +6,5 @@ vtk_fetch_module(Boolean
   "A new boolean operation filter by Ronald Romer"
   GIT_REPOSITORY https://github.com/lorensen/vtkbool
   # 26 August 2019 - linux warnings removed
-  GIT_TAG 905418b05d718df3cf217022f9a2ae55bd9a30f5
+  GIT_TAG 88de1f2fe11be9c8f245e1cb3c815bc003e18a1c
   )
