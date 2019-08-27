803f1739de8588e111a7493dea1eeb43123ccfd6
Bill Lorensen <bill.lorensen@gmail.com>
2019-08-27 00:40:16 -0400

    Trying to fix windows issues.

--- Remote/Boolean.remote.cmake
+++ Remote/Boolean.remote.cmake
@@ -6,5 +6,5 @@ vtk_fetch_module(Boolean
   "A new boolean operation filter by Ronald Romer"
   GIT_REPOSITORY https://github.com/lorensen/vtkbool
   # 26 August 2019 - linux warnings removed
-  GIT_TAG 6aef9cbe29811b014aa3754c1d83240c0a9b8f47
+  GIT_TAG 0a4546ee27e5a508ae9ab76da4211eff25a670cd
   )
