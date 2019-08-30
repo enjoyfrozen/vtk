434506ffe7ac7809ea97b59bc5f2697de6cf51c7
Bill Lorensen <bill.lorensen@gmail.com>
2019-08-30 18:53:06 -0400

    Added Unit Test.

--- Remote/Boolean.remote.cmake
+++ Remote/Boolean.remote.cmake
@@ -6,5 +6,5 @@ vtk_fetch_module(Boolean
   "A new boolean operation filter by Ronald Romer"
   GIT_REPOSITORY https://github.com/lorensen/vtkbool
   # 26 August 2019 - linux warnings removed
-  GIT_TAG a7c7829ffb8c800f1f54a79d895e0d9aceb7b1db
+  GIT_TAG 8ef8e24091747e706ce28b791b16057aa73a1d86
   )
