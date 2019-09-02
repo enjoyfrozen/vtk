de86ad82ddfd158623c6fe7cfb6e18f3f9383227
Bill Lorensen <bill.lorensen@gmail.com>
2019-09-02 17:30:24 -0400

    Added regression test

--- Remote/Boolean.remote.cmake
+++ Remote/Boolean.remote.cmake
@@ -5,6 +5,6 @@
 vtk_fetch_module(Boolean
   "A new boolean operation filter by Ronald Romer"
   GIT_REPOSITORY https://github.com/lorensen/vtkbool
-  # 01 September - fix windows linker errors
-  GIT_TAG d590b3250c7643c3a850f5111d65244ba525eac1
+  # 01 September - add regreesion test
+  GIT_TAG 123f54a49d72337860e2c7a1fe598cd1aee7baf0
   )
