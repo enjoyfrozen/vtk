800b4f47835efac9e583a50fb965d883707d8ae6
Bill Lorensen <bill.lorensen@gmail.com>
2019-09-05 19:16:46 -0400

    Fix dll linkage

--- Remote/Boolean.remote.cmake
+++ Remote/Boolean.remote.cmake
@@ -6,5 +6,5 @@ vtk_fetch_module(Boolean
   "A new boolean operation filter by Ronald Romer"
   GIT_REPOSITORY https://github.com/lorensen/vtkbool
   # 01 September - add regreesion test
-  GIT_TAG 123f54a49d72337860e2c7a1fe598cd1aee7baf0
+  GIT_TAG 98ad9b821a6e0d33b3078eaec117b7ba0e379f3f
   )
