9c8b720ac3b8a5cd8c9188728cb06300c95130b9
Bill Lorensen <bill.lorensen@gmail.com>
2019-09-09 15:42:54 -0400

    Try fixing win32 link issues

--- Remote/Boolean.remote.cmake
+++ Remote/Boolean.remote.cmake
@@ -6,5 +6,5 @@ vtk_fetch_module(Boolean
   "A new boolean operation filter by Ronald Romer"
   GIT_REPOSITORY https://github.com/lorensen/vtkbool
   # 01 September - add regreesion test
-  GIT_TAG fba5c8831a621c0fc62450f1d89a4c345c5223e5
+  GIT_TAG e74ec80b6074d273bfbc6a481011bcd4a4afe0ce
   )
