6d8aa48d106047365db931921ffafa40f3dcbef5
Sean McBride <sean@rogue-research.com>
2020-01-19 14:05:02 -0500

    RequiresExplicitFaceRepresentation, IsLinear, IsExplicitCell VTK_FUTURE_CONST

--- Common/DataModel/vtkGenericCell.cxx
+++ Common/DataModel/vtkGenericCell.cxx
@@ -113,7 +113,7 @@ int vtkGenericCell::GetCellDimension()
 }
 
 //------------------------------------------------------------------------------
-int vtkGenericCell::IsLinear()
+int vtkGenericCell::IsLinear() VTK_FUTURE_CONST
 {
   return this->Cell->IsLinear();
 }
@@ -125,7 +125,7 @@ int vtkGenericCell::RequiresInitialization()
 }
 
 //------------------------------------------------------------------------------
-int vtkGenericCell::RequiresExplicitFaceRepresentation()
+int vtkGenericCell::RequiresExplicitFaceRepresentation() VTK_FUTURE_CONST
 {
   return this->Cell->RequiresExplicitFaceRepresentation();
 }
