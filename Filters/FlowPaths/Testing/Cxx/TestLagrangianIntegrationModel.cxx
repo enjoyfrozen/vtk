98c54fc8be1404e78bb0ab9a2becc0ed58789176
Mathieu Westphal <mathieu.westphal@kitware.com>
2020-03-20 10:46:23 +0000

    Multiple improvements to LPT
    * Removed vtkLagrangianBasicIntegrationModel->SharedWeights vector as the weights are all on the vtkLagrangianThreadedData class now. Instead I use just an int to count the size needed.
    * All overloaded instances of FindInLocators use weights on the ThreadedData and should all be thread safe.
    * vtkLagrangianThreadedData stores the LastDataSetIndex as an int rather than pointers to the dataset and locator.
    * Modified InitializeThreadedData & FinalizeThreadedData on the IntegerationModel to allocate and delete vtkLagrangianThreadedData.
    * Removed the void UserData on vtkLagrangianThreadedData as its not needed.
    * Removed all useless CellLenght computation mode

--- Filters/FlowPaths/Testing/Cxx/TestLagrangianIntegrationModel.cxx
+++ Filters/FlowPaths/Testing/Cxx/TestLagrangianIntegrationModel.cxx
@@ -172,11 +172,9 @@ int TestLagrangianIntegrationModel(int, char*[])
   }
   odeWavelet->SetUseInitialIntegrationTime(false);
 
-  std::unique_ptr<vtkLagrangianThreadedData> data(new vtkLagrangianThreadedData);
-  vtkLagrangianParticle part(nvar, seedIdx, seedIdx, 0, 0, pd, odeWavelet->GetWeightsSize(), 3);
-  std::unique_ptr<vtkBilinearQuadIntersection> bqi(new vtkBilinearQuadIntersection);
-  data->BilinearQuadIntersection = bqi.get();
-  part.SetThreadedData(data.get());
+  vtkLagrangianThreadedData* data = odeWavelet->InitializeThreadedData();
+  vtkLagrangianParticle part(nvar, seedIdx, seedIdx, 0, 0, pd, 3);
+  part.SetThreadedData(data);
 
   odeWavelet->InitializeParticleData(pd);
   odeWavelet->InsertParticleData(&part, pd, 0);
@@ -663,5 +661,7 @@ int TestLagrangianIntegrationModel(int, char*[])
               << std::endl;
     return EXIT_FAILURE;
   }
+
+  odeWavelet->FinalizeThreadedData(data);
   return EXIT_SUCCESS;
 }
