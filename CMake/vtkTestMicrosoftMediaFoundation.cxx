7a99ba50a2133aa10a8f4ebba9a47e1666043070
Cory Quammen <cory.quammen@kitware.com>
2020-08-19 22:21:29 -0400

    Add configuration test for Media Foundation
    Check that Microsoft Media Foundation is available
    and add an option (default enabled) to use it
    to build vtkMP4Writer.

---
+++ CMake/vtkTestMicrosoftMediaFoundation.cxx
@@ -0,0 +1,12 @@
+#include "windows.h"
+#if 0
+#include <mfidl.h> // must be included first
+
+#include <Mfreadwrite.h>
+#include <mfapi.h>
+#include <mferror.h>
+#endif
+int main()
+{
+  return 0;
+}
