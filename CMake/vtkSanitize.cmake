a3340a5bdd57024570d28783a414f7a0c2449114
Ben Boeckel <ben.boeckel@kitware.com>
2021-01-29 16:20:33 -0500

    cmake: add options for handling sanitizer builds

---
+++ CMake/vtkSanitize.cmake
@@ -0,0 +1,52 @@
+#=========================================================================
+#
+#  This software is distributed WITHOUT ANY WARRANTY; without even
+#  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.  See the above copyright notice for more information.
+#
+#=========================================================================
+
+# This code has been adapted from smtk (https://gitlab.kitware.com/cmb/smtk)
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR
+   CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
+  set(CMAKE_COMPILER_IS_CLANGXX 1)
+endif()
+
+if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_CLANGXX)
+  #Add option for enabling sanitizers
+  option(VTK_ENABLE_SANITIZER "Build with sanitizer support." OFF)
+  mark_as_advanced(VTK_ENABLE_SANITIZER)
+
+  if(VTK_ENABLE_SANITIZER)
+    set(VTK_SANITIZER "address"
+      CACHE STRING "The sanitizer to use")
+    mark_as_advanced(VTK_SANITIZER)
+
+    if (UNIX AND NOT APPLE)
+      # Tests using external binaries need additional help to load the ASan
+      # runtime when in use.
+      if (VTK_SANITIZER MATCHES "address")
+        find_library(VTK_ASAN_LIBRARY
+          NAMES libasan.so.5
+          DOC "ASan library")
+        mark_as_advanced(VTK_ASAN_LIBRARY)
+
+        set(_vtk_testing_ld_preload
+          "${VTK_ASAN_LIBRARY}")
+      endif ()
+
+      # Tests using external binaries need additional help to load the UBSan
+      # runtime when in use.
+      if (VTK_SANITIZER MATCHES "undefined")
+        find_library(VTK_UBSAN_LIBRARY
+          NAMES libubsan.so.1
+          DOC "UBSan library")
+        mark_as_advanced(VTK_UBSAN_LIBRARY)
+
+        set(_vtk_testing_ld_preload
+          "${VTK_UBSAN_LIBRARY}")
+      endif ()
+    endif ()
+  endif()
+endif()
