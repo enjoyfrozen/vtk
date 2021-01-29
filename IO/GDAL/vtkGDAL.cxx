280101c48ca977158e66148abff191581226111a
Ben Boeckel <ben.boeckel@kitware.com>
2021-01-29 17:45:35 -0500

    vtkGDAL: add missing `vtkStandardNewMacro`

--- IO/GDAL/vtkGDAL.cxx
+++ IO/GDAL/vtkGDAL.cxx
@@ -16,6 +16,9 @@ PURPOSE.  See the above copyright notice for more information.
 #include "vtkInformationIntegerVectorKey.h"
 #include "vtkInformationKey.h"
 #include "vtkInformationStringKey.h"
+#include "vtkObjectFactory.h"
+
+vtkStandardNewMacro(vtkGDAL);
 
 vtkInformationKeyMacro(vtkGDAL, MAP_PROJECTION, String);
 vtkInformationKeyRestrictedMacro(vtkGDAL, FLIP_AXIS, IntegerVector, 3);
