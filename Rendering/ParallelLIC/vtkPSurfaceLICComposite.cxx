365b44255dec3d3b8f02f905c08978cd2db96601
Ben Boeckel <ben.boeckel@kitware.com>
2021-03-03 14:18:32 -0500

    clang-tidy: fix `modernize-use-emplace` lints

--- Rendering/ParallelLIC/vtkPSurfaceLICComposite.cxx
+++ Rendering/ParallelLIC/vtkPSurfaceLICComposite.cxx
@@ -1040,8 +1040,7 @@ int vtkPSurfaceLICComposite::BuildProgram(float* vectors)
 
             if (!sharedExt.Empty())
             {
-              this->ScatterProgram.push_back(vtkPPixelTransfer(
-                srcRank, this->WindowExt, sharedExt, destRank, this->WindowExt, sharedExt, id));
+              this->ScatterProgram.emplace_back(srcRank, this->WindowExt, sharedExt, destRank, this->WindowExt, sharedExt, id);
             }
             id += 1;
           }
@@ -1110,10 +1109,10 @@ int vtkPSurfaceLICComposite::BuildProgram(float* vectors)
           {
             // to move vectors for the LIC decomp
             // into a contiguous recv buffer
-            this->GatherProgram.push_back(
-              vtkPPixelTransfer(srcRank, this->WindowExt, sharedExt, destRank,
+            this->GatherProgram.emplace_back(
+              srcRank, this->WindowExt, sharedExt, destRank,
                 sharedExt, // dest ext
-                sharedExt, id));
+                sharedExt, id);
           }
 
           id += 1;
