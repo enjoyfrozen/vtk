513df5a65db2b430c4148423dce1d503d1d14741
Cory Quammen <cory.quammen@kitware.com>
2021-03-09 12:16:41 -0500

    Add member function to clear out text added via AddText()

--- IO/Image/vtkPNGWriter.cxx
+++ IO/Image/vtkPNGWriter.cxx
@@ -388,3 +388,9 @@ void vtkPNGWriter::AddText(const char* key, const char* value)
   impl->TextKeyValue[index].second.assign(value);
   this->Modified();
 }
+
+void vtkPNGWriter::ClearText()
+{
+  vtkInternals* impl = this->Internals;
+  impl->TextKeyValue.clear();
+}
