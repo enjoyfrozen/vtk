217e05584799b253d1430db7b22019155be4a203
Ben Boeckel <ben.boeckel@kitware.com>
2021-04-14 07:35:52 -0400

    clang-tidy: simplify comparisons with bool literals
    This fixes instances `readability-simplify-boolean-expr` lints where
    comparing with a literal bool was performed.

--- Charts/Core/vtkChartXY.cxx
+++ Charts/Core/vtkChartXY.cxx
@@ -736,7 +736,7 @@ void vtkChartXY::RecalculatePlotBounds()
   double bounds[4] = { 0.0, 0.0, 0.0, 0.0 };
   for (it = this->ChartPrivate->plots.begin(); it != this->ChartPrivate->plots.end(); ++it)
   {
-    if ((*it)->GetVisible() == false)
+    if (!(*it)->GetVisible())
     {
       continue;
     }
