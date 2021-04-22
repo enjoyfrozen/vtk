bb067983c16ec3696397fda781781978916998ce
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2021-04-22 09:03:05 -0400

    WIP: CI testing

--- GUISupport/QtQuick/Testing/Cxx/TestQQuickVTKRenderItemWidget.cxx
+++ GUISupport/QtQuick/Testing/Cxx/TestQQuickVTKRenderItemWidget.cxx
@@ -63,7 +63,7 @@ public:
     vtkImplicitPlaneRepresentation* rep =
       reinterpret_cast<vtkImplicitPlaneRepresentation*>(planeWidget->GetRepresentation());
     rep->GetPlane(this->Plane);
-    this->Actor->VisibilityOn();
+    // this->Actor->VisibilityOn();
   }
   TestQQuickVTKRenderItemWidgetCallback()
     : Plane(nullptr)
@@ -101,7 +101,7 @@ int TestQQuickVTKRenderItemWidget(int argc, char* argv[])
   vtkNew<vtkConeSource> cone;
   mapper->SetInputConnection(cone->GetOutputPort());
   actor->SetMapper(mapper);
-  coneItem->renderer()->AddActor(actor);
+  // coneItem->renderer()->AddActor(actor);
   coneItem->renderer()->SetBackground(0.5, 0.5, 0.7);
   coneItem->renderer()->SetBackground2(0.7, 0.7, 0.7);
   coneItem->renderer()->SetGradientBackground(true);
@@ -142,6 +142,8 @@ int TestQQuickVTKRenderItemWidget(int argc, char* argv[])
 
   vtkNew<vtkPolyDataMapper> selectMapper;
   selectMapper->SetInputConnection(clipper->GetOutputPort());
+  selectMapper->SetScalarModeToUsePointData();
+  selectMapper->SetScalarVisibility(false);
 
   vtkNew<vtkActor> selectActor;
   selectActor->SetMapper(selectMapper);
@@ -165,7 +167,7 @@ int TestQQuickVTKRenderItemWidget(int argc, char* argv[])
   qquickVTKWidget->setWidget(planeWidget);
   qquickVTKWidget->setEnabled(true);
 
-  widgetItem->renderer()->AddActor(maceActor);
+  // widgetItem->renderer()->AddActor(maceActor);
   widgetItem->renderer()->AddActor(selectActor);
   widgetItem->addWidget(qquickVTKWidget);
   widgetItem->update();
