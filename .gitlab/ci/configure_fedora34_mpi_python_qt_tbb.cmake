9e5e8209900e81f8c21ab8f13b4c5fef65a81a77
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2021-09-06 16:58:29 -0400

    Updated Fedora dockerfile to Fedora34

--- .gitlab/ci/configure_fedora34_mpi_python_qt_tbb.cmake
+++ .gitlab/ci/configure_fedora34_mpi_python_qt_tbb.cmake
@@ -5,4 +5,4 @@ set(VTK_MODULE_USE_EXTERNAL_VTK_libharu OFF CACHE BOOL "")
 set(VTK_MODULE_USE_EXTERNAL_VTK_exprtk OFF CACHE BOOL "")
 set(VTK_MODULE_USE_EXTERNAL_VTK_ioss OFF CACHE BOOL "")
 
-include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora33.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora34.cmake")
