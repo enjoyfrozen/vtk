216eb21c8c2f8ef397712eb5521e8a85cfd48251
Caitlin Ross <caitlin.ross@kitware.com>
2021-09-20 10:42:21 -0400

    new ensight gold reader: add test

---
+++ IO/EnSight/Testing/Python/NewEnSightGoldASCII.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import vtk
+from vtk.util.misc import vtkGetDataRoot
+VTK_DATA_ROOT = vtkGetDataRoot()
+
+# create a rendering window and renderer
+ren1 = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren1)
+renWin.SetSize(300,300)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+reader = vtk.vtkNewEnSightGoldReader()
+cdp = vtk.vtkCompositeDataPipeline()
+reader.SetDefaultExecutivePrototype(cdp)
+reader.SetCaseFileName("" + str(VTK_DATA_ROOT) + "/Data/EnSight/new-ensight-gold-test-ascii.case")
+geom0 = vtk.vtkGeometryFilter()
+geom0.SetInputConnection(reader.GetOutputPort())
+mapper0 = vtk.vtkHierarchicalPolyDataMapper()
+mapper0.SetInputConnection(geom0.GetOutputPort())
+actor0 = vtk.vtkActor()
+actor0.SetMapper(mapper0)
+# assign our actor to the renderer
+ren1.AddActor(actor0)
+# enable user interface interactor
+iren.Initialize()
+# prevent the tk window from showing up then start the event loop
+reader.SetDefaultExecutivePrototype(None)
