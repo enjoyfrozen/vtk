c68e22b3e72d999db7acf0cacb4bd1c2f634e37b
Ben Boeckel <ben.boeckel@kitware.com>
2021-10-17 20:15:10 -0400

    clang-tidy: fix `readability-redundant-member-init` lints

--- Rendering/Context2D/vtkContextScenePrivate.h
+++ Rendering/Context2D/vtkContextScenePrivate.h
@@ -44,8 +44,7 @@ public:
    * Default constructor.
    */
   vtkContextScenePrivate(vtkAbstractContextItem* item)
-    : std::vector<vtkAbstractContextItem*>()
-    , Scene(nullptr)
+    : Scene(nullptr)
     , Item(item)
   {
   }
