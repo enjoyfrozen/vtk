0e00f12d2668f9c10914acef18547364af842078
Tomohiro Ichinose <ichinose.tomohiro.sh@gmail.com>
2021-10-29 17:49:56 +0900

    Replace `assert` in test code with `vtkErrorWithObjectMacro`

--- IO/LSDyna/Testing/Cxx/TestLSDynaReaderUserIds.cxx
+++ IO/LSDyna/Testing/Cxx/TestLSDynaReaderUserIds.cxx
@@ -25,10 +25,10 @@
 #include "vtkNew.h"
 #include "vtkPointData.h"
 #include "vtkPointSet.h"
+#include "vtkSetGet.h"
 #include "vtkTestUtilities.h"
 
 #include <algorithm>
-#include <cassert>
 #include <fstream>
 #include <string>
 #include <tuple>
@@ -58,7 +58,10 @@ std::unordered_map<std::size_t, std::tuple<ElemIds, NodeIds>> ReadIdsFromDynaInp
   std::unordered_map<std::size_t, std::tuple<ElemIds, NodeIds>> results;
 
   std::ifstream ifs(fpath);
-  assert("pre: open LS-DYNA input" && ifs.is_open());
+  if (!ifs.is_open())
+  {
+    vtkErrorWithObjectMacro(nullptr, "failed to open LS-DYNA input");
+  }
 
   std::string line;
   std::string block{ "" };
@@ -87,7 +90,10 @@ std::unordered_map<std::size_t, std::tuple<ElemIds, NodeIds>> ReadIdsFromDynaInp
     else if (block.substr(0, 8) == "ELEMENT_")
     {
       auto splitted = split(line);
-      assert("pre: has eid, pid and connectivities" && splitted.size() > 2);
+      if (splitted.size() <= 2)
+      {
+        vtkErrorWithObjectMacro(nullptr, "wrong input: eid, pid and connectivities are expected");
+      }
 
       const std::size_t eid = std::stoull(splitted[0]);
       const std::size_t pid = std::stoull(splitted[1]);
@@ -101,8 +107,10 @@ std::unordered_map<std::size_t, std::tuple<ElemIds, NodeIds>> ReadIdsFromDynaInp
       for (std::size_t i = 2; i < splitted.size(); i++)
       {
         auto nid = std::stoull(splitted[i]);
-        assert("pre: nid is in global nodes" &&
-          std::find(nodeid_glob.begin(), nodeid_glob.end(), nid) != nodeid_glob.end());
+        if (std::find(nodeid_glob.begin(), nodeid_glob.end(), nid) == nodeid_glob.end())
+        {
+          vtkErrorWithObjectMacro(nullptr, "wrong input: nid must be in the global nodes");
+        }
         std::get<1>(res).emplace(nid);
       }
     }
@@ -112,7 +120,10 @@ std::unordered_map<std::size_t, std::tuple<ElemIds, NodeIds>> ReadIdsFromDynaInp
 
 std::size_t GetPartId(const std::string& pname)
 {
-  assert("pre: expected part name" && pname.substr(0, 4) == "Part");
+  if (pname.substr(0, 4) != "Part")
+  {
+    vtkErrorWithObjectMacro(nullptr, << "unexpected part name: " << pname);
+  }
   return std::stoull(pname.substr(4));
 }
 
@@ -153,8 +164,14 @@ int TestLSDynaReaderUserIds(int argc, char* argv[])
     auto cell_data = block->GetCellData();
     auto point_data = block->GetPointData();
 
-    assert("pre: has user elem ID" && cell_data->HasArray(name_elem_id));
-    assert("pre: has user node ID" && point_data->HasArray(name_node_id));
+    if (!cell_data->HasArray(name_elem_id))
+    {
+      vtkErrorWithObjectMacro(nullptr, << "data not found: user elem ID (" << name_elem_id << ")");
+    }
+    if (!point_data->HasArray(name_node_id))
+    {
+      vtkErrorWithObjectMacro(nullptr, << "data not found: user node ID (" << name_node_id << ")");
+    }
 
     auto arr_elem_id = cell_data->GetAbstractArray(name_elem_id);
     auto arr_node_id = point_data->GetAbstractArray(name_node_id);
@@ -169,25 +186,48 @@ int TestLSDynaReaderUserIds(int argc, char* argv[])
       const auto& expected_eid = std::get<0>(item);
       const auto& expected_nid = std::get<1>(item);
 
-      assert("post: same length for elem IDs" &&
-        expected_eid.size() == arr_elem_id->GetNumberOfTuples());
-      assert("post: same length for node IDs" &&
-        expected_nid.size() == arr_node_id->GetNumberOfTuples());
+      if (expected_eid.size() != arr_elem_id->GetNumberOfTuples())
+      {
+        vtkErrorWithObjectMacro(nullptr,
+          "length mismatch for elem IDs: " << expected_eid.size() << ", "
+                                           << arr_elem_id->GetNumberOfTuples());
+      }
+      if (expected_nid.size() != arr_node_id->GetNumberOfTuples())
+      {
+        vtkErrorWithObjectMacro(nullptr,
+          "length mismatch for node IDs: " << expected_nid.size() << ", "
+                                           << arr_node_id->GetNumberOfTuples());
+      }
 
       for (std::size_t i = 0; i < arr_elem_id->GetNumberOfTuples(); i++)
       {
         auto v = arr_elem_id->GetVariantValue(i);
-        assert("post: correct type for elem ID" && v.IsLongLong());
-        assert("post: user elem ID value is expected" && v.ToLongLong() == expected_eid.at(i));
+        if (!v.IsLongLong())
+        {
+          vtkErrorWithObjectMacro(
+            nullptr, << "wrong type for " << name_elem_id << "  (expected long long)");
+        }
+        if (v.ToLongLong() != expected_eid.at(i))
+        {
+          vtkErrorWithObjectMacro(nullptr, << "value mismatch for user elem ID (" << i << "): "
+                                           << v.ToLongLong() << ", " << expected_eid.at(i));
+        }
       }
 
       for (std::size_t i = 0; i < arr_node_id->GetNumberOfTuples(); i++)
       {
         // TODO: check the order of node IDs
         auto v = arr_node_id->GetVariantValue(i);
-        assert("post: correct type for node ID" && v.IsLongLong());
-        assert("post: user node ID value is expected" &&
-          expected_nid.find(v.ToLongLong()) != expected_nid.end());
+        if (!v.IsLongLong())
+        {
+          vtkErrorWithObjectMacro(
+            nullptr, << "wrong type for " << name_node_id << "  (expected long long)");
+        }
+        if (expected_nid.find(v.ToLongLong()) == expected_nid.end())
+        {
+          vtkErrorWithObjectMacro(
+            nullptr, << "unexpected value for user elem ID (" << i << "): " << v.ToLongLong());
+        }
       }
     }
   }
