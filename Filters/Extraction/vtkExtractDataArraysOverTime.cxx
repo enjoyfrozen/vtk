b890cd5d5eb38ba9fd61762ecc0dd9b41e76accd
Ben Boeckel <ben.boeckel@kitware.com>
2021-12-06 14:13:14 -0500

    vtkExtractDataArraysOverTime: remove vtkWeakPointer usage
    There is no benefit over a raw pointer because the class always exists
    for as long as its owner. Additionally, make it `const` so that it isn't
    changed accidentally.

--- Filters/Extraction/vtkExtractDataArraysOverTime.cxx
+++ Filters/Extraction/vtkExtractDataArraysOverTime.cxx
@@ -34,7 +34,7 @@
 #include "vtkSplitColumnComponents.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
 #include "vtkTable.h"
-#include "vtkWeakPointer.h"
+#include "vtkWeakPtr.h"
 
 #include <algorithm>
 #include <cassert>
@@ -121,7 +121,7 @@ private:
   typedef std::map<vtkKey, vtkValue> MapType;
   MapType OutputGrids;
   int NumberOfTimeSteps;
-  vtkWeakPointer<vtkExtractDataArraysOverTime> Self;
+  vtkExtractDataArraysOverTime* const Self;
   // We use the same time array for all extracted time lines, since that doesn't
   // change.
   vtkSmartPointer<vtkDoubleArray> TimeArray;
