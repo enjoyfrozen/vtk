b90f4b33948b36719f1566dee353f2da80ec6bd5
Ben Boeckel <ben.boeckel@kitware.com>
2021-12-15 14:42:36 -0500

    gitlab-ci: use Fedora 35 image

--- .gitlab/ci/configure_fedora35_mpi_cuda_offscreen_osmesa.cmake
+++ .gitlab/ci/configure_fedora35_mpi_cuda_offscreen_osmesa.cmake
@@ -5,4 +5,4 @@ set(VTK_MODULE_ENABLE_VTK_fides NO CACHE STRING "")
 # Lowest-common denominator.
 set(VTKm_CUDA_Architecture "pascal" CACHE STRING "")
 
-include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora34.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora35.cmake")
