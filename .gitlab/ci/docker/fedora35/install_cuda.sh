ab04ac555b38f68180aa4c2cfb5c217f651f682f
Ben Boeckel <ben.boeckel@kitware.com>
2021-12-16 15:32:42 -0500

    ci: install CUDA 11.6

--- .gitlab/ci/docker/fedora35/install_cuda.sh
+++ .gitlab/ci/docker/fedora35/install_cuda.sh
@@ -8,10 +8,10 @@ dnf install -y --setopt=install_weak_deps=False \
 
 # Install the CUDA repository.
 dnf config-manager --add-repo \
-    https://developer.download.nvidia.com/compute/cuda/repos/fedora34/x86_64/cuda-fedora34.repo
+    https://developer.download.nvidia.com/compute/cuda/repos/fedora35/x86_64/cuda-fedora35.repo
 
 # CUDA toolchain
 dnf install -y --setopt=install_weak_deps=False \
-   cuda-compiler-11-4 cuda-cudart-devel-11-4 cuda-toolkit-11-4
+   cuda-compiler-11-6 cuda-cudart-devel-11-6 cuda-toolkit-11-6
 
 dnf clean all
