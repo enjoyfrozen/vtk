e11f41580e26e612066435bb76cf3ddab5619ca7
Ben Boeckel <ben.boeckel@kitware.com>
2022-01-18 10:50:02 -0500

    ci: disable `isnan` for the Intel build

--- .gitlab/ci/configure_el8_icc_mpi_python.cmake
+++ .gitlab/ci/configure_el8_icc_mpi_python.cmake
@@ -1,1 +1,5 @@
+# `isnan` returns `0` for values which print `nan`
+set(VTK_HAS_STD_ISNAN FALSE CACHE BOOL "")
+set(VTK_HAS_ISNAN FALSE CACHE BOOL "")
+
 include("${CMAKE_CURRENT_LIST_DIR}/configure_el8.cmake")
