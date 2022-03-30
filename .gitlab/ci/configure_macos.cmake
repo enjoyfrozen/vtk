3ae5e5d6f852f726b66981061118e3ac05b531b2
Nicolas Vuaille <nicolas.vuaille@kitware.com>
2022-03-30 17:10:30 +0200

    Bypass qtquick in macos ci

--- .gitlab/ci/configure_macos.cmake
+++ .gitlab/ci/configure_macos.cmake
@@ -23,6 +23,8 @@ set(VTK_MODULE_ENABLE_VTK_RenderingRayTracing NO CACHE STRING "") # ospray
 set(VTK_MODULE_ENABLE_VTK_fides NO CACHE STRING "") # adios
 set(VTK_MODULE_ENABLE_VTK_xdmf3 NO CACHE STRING "") # boost
 
+set(VTK_MODULE_ENABLE_VTK_GUISupportQtQuick NO CACHE STRING "")
+
 # Add rpath entries for Xcode frameworks.
 set(CMAKE_BUILD_RPATH "$ENV{DEVELOPER_DIR}/Library/Frameworks" CACHE STRING "")
 set(CMAKE_INSTALL_RPATH "$ENV{DEVELOPER_DIR}/Library/Frameworks" CACHE STRING "")
