c889d9f033bc6b7aa9fbae5d0d30fd383d99b887
Nicolas Vuaille <nicolas.vuaille@kitware.com>
2022-03-30 15:27:14 +0200

    Enable Catalyst only when available

--- .gitlab/ci/configure_el8.cmake
+++ .gitlab/ci/configure_el8.cmake
@@ -1,3 +1,4 @@
-include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora_common.cmake")
 # catalyst is not installed on el8 image
 set(VTK_ENABLE_CATALYST OFF CACHE BOOL "")
+
+include("${CMAKE_CURRENT_LIST_DIR}/configure_fedora_common.cmake")
