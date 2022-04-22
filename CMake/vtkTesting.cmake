d13a42f97fa041255f0919eee3302122c52d692e
David Gobbi <david.gobbi@gmail.com>
2022-04-22 13:56:33 -0600

    Raise an error if external data store is needed
    The default location SOURCE_DIR/.ExternalData for external data exists in
    the VTK git repository but not in the release tarballs.  For builds from
    tarballs, cmake used to silently turn off VTK_BUILD_TESTING because of this.
    Now, cmake raises FATAL_ERROR and asks for an external data directory to be
    specified.

--- CMake/vtkTesting.cmake
+++ CMake/vtkTesting.cmake
@@ -4,16 +4,26 @@ set_property(CACHE VTK_BUILD_TESTING
   PROPERTY
     STRINGS "ON;OFF;WANT")
 
-if (NOT EXISTS "${VTK_SOURCE_DIR}/.ExternalData/README.rst")
-  # This file is always present in version-controlled source trees
-  # so we must have been extracted from a source tarball with no
-  # data objects needed for testing.  Turn off tests by default
-  # since enabling them requires network access or manual data
-  # store configuration.
-  set(VTK_BUILD_TESTING OFF)
-endif ()
-
 if (VTK_BUILD_TESTING)
+  if (NOT VTK_DATA_STORE)
+    # These checks must be synchronized with vtkExternalData.cmake
+    if (NOT EXISTS "${VTK_SOURCE_DIR}/.ExternalData/README.rst" AND
+        NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/../VTKExternalData" AND
+        NOT IS_DIRECTORY "${CMAKE_SOURCE_DIR}/../ExternalData" AND
+        NOT DEFINED "ENV{VTKExternalData_OBJECT_STORES}" AND
+        NOT DEFINED "ENV{ExternalData_OBJECT_STORES}")
+
+      # The file .ExternalData/README.rst exists in the VTK git repository
+      # but is not included in the VTK release tarballs.  For them, the data
+      # directory is expected to exist outside of the source tree.
+      message(FATAL_ERROR "VTK_BUILD_TESTING is ${VTK_BUILD_TESTING}, but no "
+              "external data directory has been specified! Please create a "
+              "directory called \"ExternalData\" or \"VTKExternalData\" at "
+              "the same level as the VTK source tree, or set VTK_DATA_STORE "
+              "to the name of the desired data directory.")
+    endif ()
+  endif ()
+
   include(vtkExternalData)
   include(CTest)
   set_property(CACHE BUILD_TESTING
