a6d586d591a124eaad9e47cfb6ed5bda446a0149
Spiros Tsalikis <spiros.tsalikis@kitware.com>
2022-05-03 20:51:48 -0400

    vtkCellTreeLocator: Implement SupportLinearTransformation, Shallow Copy

--- Common/DataModel/vtkCellTreeLocator.h
+++ Common/DataModel/vtkCellTreeLocator.h
@@ -29,6 +29,7 @@
  * - NumberOfCellsPerNode        (default 8)
  * - CacheCellBounds             (default true)
  * - UseExistingSearchStructure  (default false)
+ * - SupportLinearTransformation (default false)
  *
  * vtkCellTreeLocator does NOT utilize the following parameters:
  * - Automatic
@@ -50,7 +51,9 @@
 #include "vtkAbstractCellLocator.h"
 #include "vtkCommonDataModelModule.h" // For export macro
 #include "vtkDeprecation.h"           // For VTK_DEPRECATED_IN_9_2_0
-#include <vector>                     // Needed for internal class
+
+#include <memory> // Needed for shared_ptr
+#include <vector> // Needed for internal class
 
 class vtkCellPointTraversal;
 class vtkIdTypeArray;
@@ -107,8 +110,9 @@ public:
 
   /**
    * Return a list of unique cell ids inside of a given bounding box. The
-   * user must provide the vtkIdList to populate. This method returns data
-   * only after the locator has been built.
+   * user must provide the vtkIdList to populate.
+   *
+   * This function does NOT work when SupportLinearTransformation is on.
    */
   void FindCellsWithinBounds(double* bbox, vtkIdList* cells) override;
 
@@ -205,6 +209,11 @@ public:
     vtkIdType Size() const;
   };
 
+  /**
+   * Shallow copy of a vtkCellTreeLocator. Useful when SupportLinearTransformation is on.
+   */
+  void ShallowCopy(vtkLocator* locator) override;
+
 protected:
   vtkCellTreeLocator();
   ~vtkCellTreeLocator() override;
@@ -219,7 +228,7 @@ protected:
 
   int NumberOfBuckets;
 
-  vtkCellTree* Tree;
+  std::shared_ptr<vtkCellTree> Tree;
 
   friend class vtkCellPointTraversal;
   friend class vtkCellTreeNode;
