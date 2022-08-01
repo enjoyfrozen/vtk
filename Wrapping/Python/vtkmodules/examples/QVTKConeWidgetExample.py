dc13fab6737f8126734da5ead0a0768b332817e9
Adam Hendry <adam.grant.hendry@gmail.com>
2022-08-01 12:39:12 -0700

    feat(info): add function to print OS and graphics info in example

--- Wrapping/Python/vtkmodules/examples/QVTKConeWidgetExample.py
+++ Wrapping/Python/vtkmodules/examples/QVTKConeWidgetExample.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import sys
+
+import qtpy
 import vtkmodules.vtkInteractionStyle  # noqa
 import vtkmodules.vtkRenderingOpenGL2  # noqa
 from qtpy import QtGui, QtWidgets
@@ -7,6 +10,57 @@ from vtkmodules.qt.QVTKOpenGLNativeWidget import QVTKOpenGLNativeWidget
 from vtkmodules.vtkFiltersSources import vtkConeSource
 from vtkmodules.vtkRenderingCore import vtkActor, vtkPolyDataMapper, vtkRenderer
 from vtkmodules.vtkRenderingOpenGL2 import vtkGenericOpenGLRenderWindow
+
+try:
+    from OpenGL import GL
+except ImportError:
+    raise ImportError(
+        "PyOpenGL must be installed to run this example. To install, run"
+        "'pip install PyOpenGL PyOpenGL_accelerate'"
+    )
+
+OS = {
+    'win32': 'Windows',
+    'darwin': 'MacOS',
+    'aix': 'AIX',
+    'linux': 'Linux',
+    'cygwin': 'Windows with Cygwin',
+}
+
+
+def print_info() -> str:
+    python_major, python_minor, python_patch, _, _ = sys.version_info
+    os_ = OS.get(sys.platform, sys.platform)
+
+    print(f'OS: {os_}')
+    if os_ in {'Windows', 'Windows with Cygwin'}:
+        windows_major, windows_minor, windows_build, _, _ = sys.getwindowsversion()
+        print(f'OS Version: {windows_major}.{windows_minor}, Build {windows_build}')
+    print(f'Python: {python_major}.{python_minor}.{python_patch}')
+    print(f'Qt Binding: {qtpy.API}')
+    print(f'Qt Version: {qtpy.QT_VERSION}')
+
+    context = QtGui.QOpenGLContext.currentContext()
+    f = context.functions()
+    format_ = context.format()
+
+    gl_vendor = f.glGetString(GL.GL_VENDOR)
+    gl_renderer = f.glGetString(GL.GL_RENDERER)
+    gl_version = f.glGetString(GL.GL_VERSION)
+    gl_lang_version = f.glGetString(GL.GL_SHADING_LANGUAGE_VERSION)
+
+    gl_profile = (
+        'Core'
+        if format_.profile() is QtGui.QSurfaceFormat.CoreProfile
+        else 'Compatibility'
+    )
+    gl_profile_major = format_.majorVersion()
+    gl_profile_minor = format_.minorVersion()
+
+    print(f'Graphics Vendor: {gl_vendor}')
+    print(f'Graphics Card: {gl_renderer}')
+    print(f'Graphics Driver: {gl_version}')
+    print(f'OpenGL Profile: {gl_profile}, version {gl_profile_major}.{gl_profile_minor}')
 
 
 class ConeWidgetExample(QtWidgets.QMainWindow):
@@ -43,9 +97,12 @@ if __name__ == '__main__':
     # profile.
     # See:
     #  - https://doc.qt.io/qtforpython/PySide6/QtOpenGLWidgets/QOpenGLWidget.html#painting-techniques
+
     QtGui.QSurfaceFormat.setDefaultFormat(QVTKOpenGLNativeWidget.defaultFormat())
     app = QtWidgets.QApplication([])
     win = ConeWidgetExample()
     win.show()
+
+    print_info()
 
     app.exec()
