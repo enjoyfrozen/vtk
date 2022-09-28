39441efad77542634775d4f180bacb2edec191e8
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2022-09-27 22:51:20 -0400

    Special mapper for passing vtkmDataSet through for rendering
    This change introduces a vtkmDataSetMapper whose role is to map
    vtkmDataSets to graphics primitives. As of now, vtkmDataSetMapper is
    just a no-op implementation that gets overriden when OSPRay is enabled.
    In that case, the OSPRay view node factory replaces the mapper with the
    vtkmOSPRayDataSetMapperNode that will hand over the vtkmDataSet directly
    to OSPRay for rendering without offloading from the GPU device.

---
+++ Rendering/RayTracing/vtkmOSPRayDataSetMapperNode.h
@@ -0,0 +1,68 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkmOSPRayDataSetMapperNode.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+/**
+ * @class vtkmOSPRayDataSetMapperNode
+ * @brief
+ *
+ */
+
+#ifndef vtkmOSPRayDataSetMapperNode_h
+#define vtkmOSPRayDataSetMapperNode_h
+
+#include "vtkMapperNode.h"
+#include "vtkRenderingRayTracingModule.h" // For export macro
+
+VTK_ABI_NAMESPACE_BEGIN
+// Forward declarations
+
+class VTKRENDERINGRAYTRACING_EXPORT vtkmOSPRayDataSetMapperNode : public vtkMapperNode
+{
+public:
+  /**
+   * Instantiate the class.
+   */
+  static vtkmOSPRayDataSetMapperNode* New();
+
+  ///@{
+  /**
+   * Standard methods for the VTK class.
+   */
+  vtkTypeMacro(vtkmOSPRayDataSetMapperNode, vtkMapperNode);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+  ///@}
+
+  /**
+   * Make ospray calls to render me.
+   */
+  void Render(bool prepass) override;
+
+  /**
+   * Invalidates cached rendering data.
+   */
+  void Invalidate(bool prepass) override;
+
+protected:
+  vtkmOSPRayDataSetMapperNode() = default;
+  ~vtkmOSPRayDataSetMapperNode() = default;
+
+  // Helper members
+
+private:
+  vtkmOSPRayDataSetMapperNode(const vtkmOSPRayDataSetMapperNode&) = delete;
+  void operator=(const vtkmOSPRayDataSetMapperNode) = delete;
+};
+
+VTK_ABI_NAMESPACE_END
+#endif // vtkmOSPRayDataSetMapperNode_h
