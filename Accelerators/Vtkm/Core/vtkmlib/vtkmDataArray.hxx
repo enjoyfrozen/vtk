7fa33efcd223023486ec699047c9a1b49d6e16fe
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2022-11-01 14:36:15 -0400

    Temporarily bypass OpenMP get_default_device API issues
    The API is not found on some systems/compilers

--- Accelerators/Vtkm/Core/vtkmlib/vtkmDataArray.hxx
+++ Accelerators/Vtkm/Core/vtkmlib/vtkmDataArray.hxx
@@ -118,7 +118,7 @@ struct MetaDataFunctor
   VTKM_CONT bool operator()(
     Device, const vtkm::cont::ArrayHandle<T, Storage>& fielddata, MetaData& metadata)
   {
-    int ompdevice = omp_get_default_device();
+    int ompdevice = 0;         // omp_get_default_device();
     auto ompcontext = nullptr; // omp_target_get_context(ompdevice);
     auto buffers = fielddata.GetBuffers();
     auto numbuff = buffers.size();
