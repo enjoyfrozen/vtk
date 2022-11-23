643d9ffbc941d5474498e3a5ffd2f0de4b821690
Thomas Galland <thomas.galland@kitware.com>
2022-11-23 14:10:33 +0100

    POC 3D Cursor

---
+++ Interaction/Style/vtkInteractorStyle3DCursor.cxx
@@ -0,0 +1,116 @@
+#include "vtkActor.h"
+#include "vtkCallbackCommand.h"
+#include "vtkCollectionIterator.h"
+#include "vtkInteractorStyle3DCursor.h"
+#include "vtkObjectFactory.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+#include "vtkRenderer.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkSphereSource.h"
+
+// Pickers
+#include "vtkHardwarePicker.h"
+#include "vtkWorldPointPicker.h"
+
+VTK_ABI_NAMESPACE_BEGIN
+//------------------------------------------------------------------------------
+void PlaceCursorCallBack(vtkObject* vtkNotUsed(caller), long unsigned int vtkNotUsed(eventId),
+  void* clientData, void* vtkNotUsed(callData))
+{
+  vtkInteractorStyle3DCursor* self = reinterpret_cast<vtkInteractorStyle3DCursor*>(clientData);
+  if (self)
+  {
+    self->PlaceCursor();
+  }
+}
+
+//------------------------------------------------------------------------------
+vtkStandardNewMacro(vtkInteractorStyle3DCursor);
+
+//------------------------------------------------------------------------------
+vtkInteractorStyle3DCursor::vtkInteractorStyle3DCursor()
+{
+  this->Place3DCursor->SetCallback(PlaceCursorCallBack);
+  this->Place3DCursor->SetClientData(this);
+  // Ignore focus
+  this->Place3DCursor->SetPassiveObserver(true);
+
+  vtkNew<vtkSphereSource> sphere;
+  sphere->SetThetaResolution(6);
+  sphere->SetPhiResolution(6);
+  sphere->SetRadius(0.05);
+
+  vtkNew<vtkPolyDataMapper> mapper;
+  mapper->SetInputConnection(sphere->GetOutputPort());
+
+  this->Cursor->SetMapper(mapper);
+  this->Cursor->GetProperty()->SetColor(0.8900, 0.6600, 0.4100);
+}
+
+//------------------------------------------------------------------------------
+void vtkInteractorStyle3DCursor::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os, indent);
+}
+
+//------------------------------------------------------------------------------
+void vtkInteractorStyle3DCursor::SetInteractor(vtkRenderWindowInteractor* interactor)
+{
+  if (interactor == this->Interactor)
+  {
+    return;
+  }
+
+  this->Superclass::SetInteractor(interactor);
+
+  if (interactor)
+  {
+    // Add a new passive observer displaying the 3D cursor under the mouse
+    interactor->AddObserver(vtkCommand::MouseMoveEvent, this->Place3DCursor, this->Priority);
+
+    // Retrieve current renderer
+    this->FindPokedRenderer(0, 0);
+
+    // Fill pickable props list
+    this->Picker->PickFromListOn();
+    vtkPropCollection* propCollection = this->GetCurrentRenderer()->GetViewProps();
+    vtkCollectionSimpleIterator iterator;
+    vtkProp* prop = nullptr;
+    for (propCollection->InitTraversal(iterator); (prop = propCollection->GetNextProp(iterator));)
+    {
+      this->Picker->AddPickList(prop);
+    }
+
+    // Add cursor actor (not in the pickable list)
+    this->GetCurrentRenderer()->AddActor(this->Cursor);
+
+    // Hide cursor (DOES NOT SEEMS TO WORK HERE - vtkXOpenGLRenderWindow)
+    interactor->GetRenderWindow()->HideCursor();
+  }
+}
+
+//------------------------------------------------------------------------------
+void vtkInteractorStyle3DCursor::SetCurrentRenderer(vtkRenderer* ren)
+{
+  this->Superclass::SetCurrentRenderer(ren);
+}
+
+//------------------------------------------------------------------------------
+void vtkInteractorStyle3DCursor::PlaceCursor()
+{
+  int x = this->Interactor->GetEventPosition()[0];
+  int y = this->Interactor->GetEventPosition()[1];
+
+  this->FindPokedRenderer(x, y);
+  this->Picker->Pick(x, y, 0.0, this->GetCurrentRenderer());
+
+  double pos[3] = { 0.0 };
+  this->Picker->GetPickPosition(pos);
+  this->Cursor->SetPosition(pos);
+
+  this->Interactor->Render();
+}
+
+VTK_ABI_NAMESPACE_END
---
+++ Interaction/Style/vtkInteractorStyle3DCursor.h
@@ -0,0 +1,47 @@
+#ifndef vtkInteractorStyle3DCursor_h
+#define vtkInteractorStyle3DCursor_h
+
+#include "vtkInteractionStyleModule.h" // for export macro
+#include "vtkInteractorStyleTrackballCamera.h" // for inheritance
+#include "vtkNew.h" // for vtkNew
+
+VTK_ABI_NAMESPACE_BEGIN
+
+class vtkActor;
+class vtkCallbackCommand;
+
+// Pickers
+class vtkHardwarePicker;
+class vtkWorldPointPicker;
+
+class VTKINTERACTIONSTYLE_EXPORT vtkInteractorStyle3DCursor : public vtkInteractorStyleTrackballCamera
+{
+public:
+  static vtkInteractorStyle3DCursor* New();
+  vtkTypeMacro(vtkInteractorStyle3DCursor, vtkInteractorStyleTrackballCamera);
+  void PrintSelf(ostream& os, vtkIndent indent) override;
+
+  /**
+   * Overriden to add a new callback as passive observer
+   */
+  void SetInteractor(vtkRenderWindowInteractor* interactor) override;
+
+  void PlaceCursor();
+
+protected:
+  vtkInteractorStyle3DCursor();
+  ~vtkInteractorStyle3DCursor() override = default;
+
+  void SetCurrentRenderer(vtkRenderer*) override;
+
+  vtkNew<vtkActor> Cursor;
+  vtkNew<vtkCallbackCommand> Place3DCursor;
+  vtkNew<vtkHardwarePicker> Picker;
+
+private:
+  vtkInteractorStyle3DCursor(const vtkInteractorStyle3DCursor&) = delete;
+  void operator=(const vtkInteractorStyle3DCursor&) = delete;
+};
+VTK_ABI_NAMESPACE_END
+
+#endif // vtkInteractorStyle3DCursor_h
