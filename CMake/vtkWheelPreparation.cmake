7ab23a7d9264b686b4332a78d32d8e46c96838f6
Ben Boeckel <ben.boeckel@kitware.com>
2022-12-07 20:38:38 -0500

    WIP: Python: use the stable ABI
    This is currently completely busted because the following APIs are not
    available in this mode:
    
    Types:
    
    - `PyConfig`
    - `PyTypeObject`
    - `PyStatus`
    
    Functions (not directly associated with the types):
    
    - `Py_RunMain`
    - `PyRun_SimpleString`
    - `Py_FrozenFlag`

--- CMake/vtkWheelPreparation.cmake
+++ CMake/vtkWheelPreparation.cmake
@@ -15,8 +15,13 @@ if (UNIX AND NOT APPLE)
   endif ()
 endif ()
 
-find_package(Python3 COMPONENTS Interpreter Development.Module)
-set_property(GLOBAL PROPERTY _vtk_python_soabi "${Python3_SOABI}")
+find_package(Python3 COMPONENTS Interpreter Development.Module Development.SABIModule)
+if (Python3_VERSION VERSION_GREATER "3.8" AND
+    Python3_Development.SABIModule_FOUND)
+  set_property(GLOBAL PROPERTY _vtk_python_soabi "${Python3_SOSABI}")
+else ()
+  set_property(GLOBAL PROPERTY _vtk_python_soabi "${Python3_SOABI}")
+endif ()
 
 execute_process(
   COMMAND "${Python3_EXECUTABLE}"
