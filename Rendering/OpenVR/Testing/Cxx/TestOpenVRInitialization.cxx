ff9b6dd7b263a8afc54852ff8cf8b5fcce6efc10
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-01-06 16:58:05 +0100

    Cleanup OpenVR tests

---
+++ Rendering/OpenVR/Testing/Cxx/TestOpenVRInitialization.cxx
@@ -0,0 +1,47 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+
+#include "vtkOpenVRCamera.h"
+#include "vtkOpenVRRenderWindow.h"
+#include "vtkOpenVRRenderWindowInteractor.h"
+#include "vtkOpenVRRenderer.h"
+
+//------------------------------------------------------------------------------
+// Only initialize, requires a OpenVR implementation but do not render anything
+int TestOpenVRInitialization(int argc, char* argv[])
+{
+  vtkNew<vtkOpenVRRenderer> renderer;
+  vtkNew<vtkOpenVRRenderWindow> renderWindow;
+  vtkNew<vtkOpenVRCamera> cam;
+  vtkNew<vtkOpenVRRenderWindowInteractor> iren;
+  vtkNew<vtkActor> actor;
+
+  renderer->SetActiveCamera(cam);
+  renderer->AddActor(actor);
+  renderWindow->AddRenderer(renderer);
+  iren->SetRenderWindow(renderWindow);
+  iren->SetActionManifestDirectory("../../");
+
+  renderWindow->Initialize();
+  if (renderWindow->GetHMD())
+  {
+    iren->Initialize();
+  }
+  else
+  {
+    return 1;
+  }
+
+  return 0;
+}
