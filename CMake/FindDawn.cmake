938605f40089f098dbbb0b4ecb2cb7bd2fec0a90
Ben Boeckel <ben.boeckel@kitware.com>
2023-04-10 09:10:19 -0400

    FindDawn: remove `NO_CACHE` and `REQUIRED` args on `find_` calls
    We want these to be cached to avoid searching every single configure.
    `FPHSA` also handles the `REQUIRED` logic (as the module could be
    searched for optionally).

--- CMake/FindDawn.cmake
+++ CMake/FindDawn.cmake
@@ -17,8 +17,8 @@ find_library(DAWN_NATIVE_LIBRARY
     libdawn_native.dll
   PATHS ${DAWN_BINARY_DIR}
   DOC "Path to libdawn_native"
-  NO_CACHE REQUIRED
 )
+mark_as_advanced(DAWN_NATIVE_LIBRARY)
 
 find_library(DAWN_PLATFORM_LIBRARY
   NAMES
@@ -26,8 +26,8 @@ find_library(DAWN_PLATFORM_LIBRARY
     libdawn_platform.dll
   PATHS ${DAWN_BINARY_DIR}
   DOC "Path to libdawn_platform"
-  NO_CACHE REQUIRED
 )
+mark_as_advanced(DAWN_PLATFORM_LIBRARY)
 
 find_library(DAWN_PROC_LIBRARY
   NAMES
@@ -35,29 +35,29 @@ find_library(DAWN_PROC_LIBRARY
     libdawn_proc.dll
   PATHS ${DAWN_BINARY_DIR}
   DOC "Path to libdawn_proc"
-  NO_CACHE REQUIRED
 )
+mark_as_advanced(DAWN_PROC_LIBRARY)
 
 find_path(DAWN_GEN_INCLUDE_DIR
   NAMES dawn/webgpu_cpp.h
   PATHS ${DAWN_BINARY_DIR}/gen/include
   DOC "Path to generated dawn/webgpu_cpp.h"
-  NO_CACHE REQUIRED
 )
+mark_as_advanced(DAWN_GEN_INCLUDE_DIR)
 
 find_path(DAWN_GEN_SRC_DIR
   NAMES dawn/webgpu_cpp.cpp
   PATHS ${DAWN_BINARY_DIR}/gen/src
   DOC "Path to generated dawn/webgpu_cpp.cpp"
-  NO_CACHE REQUIRED
 )
+mark_as_advanced(DAWN_GEN_SRC_DIR)
 
 find_path(DAWN_INCLUDE_DIR
   NAMES dawn/dawn_proc.h
   PATHS ${DAWN_SOURCE_DIR}/include
   DOC "Path to generated dawn/dawn_proc.h"
-  NO_CACHE REQUIRED
 )
+mark_as_advanced(DAWN_INCLUDE_DIR)
 
 set(DAWN_LIBRARIES
   ${DAWN_PLATFORM_LIBRARY}
