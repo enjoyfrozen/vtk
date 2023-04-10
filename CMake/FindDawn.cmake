fbdbd72ad6ed5b5566784ecbae760c97e58771eb
Ben Boeckel <ben.boeckel@kitware.com>
2023-04-10 09:12:26 -0400

    FindDawn: remove excess newlines
    There's no open argument list here, so hoist the parenthesis up.

--- CMake/FindDawn.cmake
+++ CMake/FindDawn.cmake
@@ -15,45 +15,39 @@ find_library(DAWN_NATIVE_LIBRARY
   NAMES
     dawn_native
   PATHS ${DAWN_BINARY_DIR}
-  DOC "Path to libdawn_native"
-)
+  DOC "Path to libdawn_native")
 mark_as_advanced(DAWN_NATIVE_LIBRARY)
 
 find_library(DAWN_PLATFORM_LIBRARY
   NAMES
     dawn_platform
   PATHS ${DAWN_BINARY_DIR}
-  DOC "Path to libdawn_platform"
-)
+  DOC "Path to libdawn_platform")
 mark_as_advanced(DAWN_PLATFORM_LIBRARY)
 
 find_library(DAWN_PROC_LIBRARY
   NAMES
     dawn_proc
   PATHS ${DAWN_BINARY_DIR}
-  DOC "Path to libdawn_proc"
-)
+  DOC "Path to libdawn_proc")
 mark_as_advanced(DAWN_PROC_LIBRARY)
 
 find_path(DAWN_GEN_INCLUDE_DIR
   NAMES dawn/webgpu_cpp.h
   PATHS ${DAWN_BINARY_DIR}/gen/include
-  DOC "Path to generated dawn/webgpu_cpp.h"
-)
+  DOC "Path to generated dawn/webgpu_cpp.h")
 mark_as_advanced(DAWN_GEN_INCLUDE_DIR)
 
 find_path(DAWN_GEN_SRC_DIR
   NAMES dawn/webgpu_cpp.cpp
   PATHS ${DAWN_BINARY_DIR}/gen/src
-  DOC "Path to generated dawn/webgpu_cpp.cpp"
-)
+  DOC "Path to generated dawn/webgpu_cpp.cpp")
 mark_as_advanced(DAWN_GEN_SRC_DIR)
 
 find_path(DAWN_INCLUDE_DIR
   NAMES dawn/dawn_proc.h
   PATHS ${DAWN_SOURCE_DIR}/include
-  DOC "Path to generated dawn/dawn_proc.h"
-)
+  DOC "Path to generated dawn/dawn_proc.h")
 mark_as_advanced(DAWN_INCLUDE_DIR)
 
 set(DAWN_LIBRARIES
