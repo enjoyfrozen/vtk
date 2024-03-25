1ebd56f328ab1799c9dd9e2d914c953b2a570ecc
Yohann Bearzi <yohann.bearzi@kitware.com>
2024-03-25 11:55:58 -0400

    Correcting log message in `vtkTesting` for image compare

--- Testing/Rendering/vtkTesting.cxx
+++ Testing/Rendering/vtkTesting.cxx
@@ -802,7 +802,8 @@ int vtkTesting::RegressionTest(vtkAlgorithm* imageSource, double thresh, ostream
       vtkLog(INFO, "wass1 = [" << wass1[0] << ", " << wass1[1] << ", " << wass1[2] << "]");
       vtkLog(INFO,
         "Note: if the test fails but is visually acceptable, one can make the test pass"
-          << " by changing the method (TIGHT_VALID vs LOOSE_VALID) and the threshold in CMake.");
+          << " by changing the method (TIGHT_VALID vs LOOSE_VALID) in CMake, or changing"
+          << " the threshold with `vtkTesting` in C++ or `rtImageTest.py` in Python.");
 
       switch (imageCompareMethod)
       {
