1e01dc68babf1efbee1f53bd0a1b937f58d00c3a
David Thompson <david.thompson@kitware.com>
2024-08-01 11:53:29 -0400

    Add a test for vtkCellGridToUnstructuredGrid.

---
+++ Filters/CellGrid/Testing/Python/TestCellGridToUnstructuredGrid.py
@@ -0,0 +1,132 @@
+# SPDX-FileCopyrightText: Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+# SPDX-License-Identifier: BSD-3-Clause
+
+from vtkmodules import vtkCommonCore as cc
+from vtkmodules import vtkCommonDataModel as dm
+from vtkmodules import vtkCommonExecutionModel as em
+from vtkmodules import vtkImagingCore as ic
+from vtkmodules import vtkFiltersCellGrid as fc
+from vtkmodules import vtkIOCellGrid as io
+from vtkmodules import vtkIOXML as ix
+from vtkmodules import vtkIOIOSS as ii
+from vtkmodules import vtkCommonColor as cl
+from vtkmodules.util.vtkAlgorithm import VTKPythonAlgorithmBase
+
+from vtkmodules.vtkRenderingCore import *
+from vtkmodules.vtkRenderingOpenGL2 import *
+from vtkmodules.vtkRenderingCellGrid import *
+from vtkmodules.vtkInteractionStyle import *
+from vtkmodules.vtkInteractionWidgets import vtkCameraOrientationWidget
+
+from vtkmodules.test import Testing
+import os
+import sys
+
+# Register render responder for DG cells:
+vtkRenderingCellGrid.RegisterCellsAndResponders()
+fc.vtkFiltersCellGrid.RegisterCellsAndResponders()
+
+class TestCellGridToUnstructuredGrid(Testing.vtkTest):
+    def testCellCenters(self):
+        bds = [0, 0, 0, 0, 0, 0]
+
+        rdr = io.vtkCellGridReader()
+        rdr.SetFileName(os.path.join(Testing.VTK_DATA_ROOT, 'Data', 'dgMixed.dg'))
+
+        srf = fc.vtkCellGridComputeSides()
+        srf.SetInputConnection(rdr.GetOutputPort())
+        sid = fc.vtkCellGridComputeSides()
+        sid.SetOutputDimensionControl(dm.vtkCellGridSidesQuery.EdgesOfInputs)
+        # sid.OmitSidesForRenderableInputsOff()
+        sid.SetInputConnection(srf.GetOutputPort())
+        sid.Update()
+
+        pdc = fc.vtkCellGridToUnstructuredGrid()
+        pdc.SetInputConnection(rdr.GetOutputPort())
+
+        shapeMapper = vtkCellGridMapper()
+        shapeMapper.SetInputConnection(sid.GetOutputPort())
+        shapeMapper.ScalarVisibilityOff()
+        shapeActor = vtkActor()
+        shapeActor.SetMapper(shapeMapper)
+        sppty = vtkProperty()
+        sppty.SetOpacity(1.0)
+        sppty.SetLineWidth(1)
+        sppty.SetPointSize(8)
+        sppty.SetColor(0, 0, 0)
+        shapeActor.SetProperty(sppty)
+
+        pdc.Update()
+        dataMapper = vtkDataSetMapper()
+        dataMapper.SetInputConnection(pdc.GetOutputPort())
+        dataMapper.ScalarVisibilityOn()
+        dataMapper.SetScalarMode(VTK_SCALAR_MODE_USE_POINT_FIELD_DATA)
+        dataMapper.SelectColorArray('scalar1')
+        dataMapper.SetArrayComponent(0)
+        lkup = cc.vtkLookupTable()
+        clr = cl.vtkColorSeries(color_scheme=cl.vtkColorSeries.BREWER_QUALITATIVE_DARK2)
+        clr.BuildLookupTable(lkup, cl.vtkColorSeries.ORDINAL);
+        dataMapper.SetLookupTable(lkup)
+
+        dataActor = vtkActor()
+        dataActor.SetMapper(dataMapper)
+        cppty = vtkProperty()
+        cppty.SetOpacity(0.7)
+        cppty.SetLineWidth(5)
+        cppty.SetPointSize(8)
+        cppty.SetColor(1, 0, 0)
+        dataActor.SetProperty(cppty)
+
+        ren = vtkRenderer()
+        ren.AddActor(shapeActor)
+        ren.AddActor(dataActor)
+
+        rw = vtkRenderWindow()
+        rw.AddRenderer(ren)
+        ren.SetBackground(1.0, 1.0, 1.0)
+        #ren.SetBackground(0.5, 0.4, 0.3)
+        #ren.SetBackground2(0.7, 0.6, 0.5)
+        #ren.GradientBackgroundOn()
+
+        rwi = rw.MakeRenderWindowInteractor()# vtkRenderWindowInteractor()
+        rwi.SetRenderWindow(rw)
+
+        cow = vtkCameraOrientationWidget()
+        cow.SetParentRenderer(ren)
+        # Enable the widget.
+        cow.On()
+
+        sid.GetOutputDataObject(0).GetBounds(bds)
+        cam = ren.GetActiveCamera()
+        # If you want to change the camera location, this is a good starting point:
+        # dc = ((bds[0]+bds[1])/2, (bds[2]+bds[3])/2, (bds[4] + bds[5])/2)
+        # cam.SetFocalPoint(dc[0], dc[1], dc[2] + 1)
+        # cam.SetPosition(dc[0]+1, dc[1]+1, dc[2]-1.)
+
+        cam.SetViewUp(0., 0., -1.)
+        cam.SetPosition(1.21346, 1.24257, 5.66879)
+        cam.SetFocalPoint(0.146239, 0.394899, 0.0458881)
+        cam.SetDistance(4.63531)
+
+        if '-I' in sys.argv:
+            wri = io.vtkCellGridWriter()
+            wri.SetFileName(os.path.join(Testing.VTK_TEMP_DIR, 'cell-grid-to-unstructured.dg'))
+            wri.SetInputConnection(sid.GetOutputPort())
+            wri.Write()
+
+            wru = ix.vtkXMLUnstructuredGridWriter()
+            wru.SetFileName(os.path.join(Testing.VTK_TEMP_DIR, 'cell-grid-to-unstructured.vtu'))
+            wru.SetDataModeToAscii()
+            wru.SetInputConnection(pdc.GetOutputPort())
+            wru.Write()
+
+        rwi.Initialize()
+        rw.Render()
+        if '-I' in sys.argv:
+            rwi.Start()
+            print('camera', cam)
+        baseline = 'TestCellGridToUnstructuredGrid.png'
+        Testing.compareImage(rw, Testing.getAbsImagePath(baseline), threshold=25)
+
+if __name__ == "__main__":
+    Testing.main([(TestCellGridToUnstructuredGrid, 'test')])
