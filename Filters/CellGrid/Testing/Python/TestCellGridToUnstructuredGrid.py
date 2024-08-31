85d381a0b5f67bb0dbe3777e630129fdb97f350d
David Thompson <david.thompson@kitware.com>
2024-08-31 03:36:39 -0400

    Add baseline images for TestCellGridToUnstructuredGrid.

--- Filters/CellGrid/Testing/Python/TestCellGridToUnstructuredGrid.py
+++ Filters/CellGrid/Testing/Python/TestCellGridToUnstructuredGrid.py
@@ -39,9 +39,7 @@ class TestCellGridToUnstructuredGrid(Testing.vtkTest):
         flagEdgesOnly = dm.vtkCellGridSidesQuery.EdgesOfInputs
 
         srf = (rdr >> fc.vtkCellGridComputeSides(output_dimension_control=flagEdgesOnly)).last
-        # Swap this out once the range-responder handles sides:
-        # sid = (rdr >> fc.vtkCellGridComputeSides(output_dimension_control=flagEdgesVerts)).last
-        sid = (rdr >> fc.vtkCellGridComputeSides(output_dimension_control=flagEdgesOnly)).last
+        sid = (rdr >> fc.vtkCellGridComputeSides(output_dimension_control=flagEdgesVerts)).last
         pdc = (sid >> fc.vtkCellGridCellCenters() >> fc.vtkCellGridToUnstructuredGrid()).last
 
         shapeMapper = (sid >> vtkCellGridMapper(scalar_visibility=False)).last
@@ -49,31 +47,23 @@ class TestCellGridToUnstructuredGrid(Testing.vtkTest):
         shapeActor = vtkActor(mapper=shapeMapper, property=sppty)
 
         pdc.Update()
-        lkup = cc.vtkLookupTable()
-        clr = cl.vtkColorSeries(color_scheme=cl.vtkColorSeries.BREWER_QUALITATIVE_DARK2)
-        clr.BuildLookupTable(lkup, cl.vtkColorSeries.ORDINAL);
         arrowSource = fs.vtkArrowSource(
             tip_resolution=64,
             shaft_resolution=64,
-            # arrow_origin=fs.vtkArrowSource.ArrowOrigins.Center
         )
         glyphMapper = (pdc >> vtkGlyph3DMapper(
-            scalar_visibility=True,
-            scalar_mode=VTK_SCALAR_MODE_USE_POINT_FIELD_DATA,
             array_component=-1,
-            lookup_table=lkup,
             orient=True,
             orientation_array='curl1',
             source_connection=arrowSource.GetOutputPort(),
             scaling=True,
             scale_mode=vtkGlyph3DMapper.SCALE_BY_MAGNITUDE,
             scale_array='curl1',
-            scale_factor=1.,
+            scale_factor=0.25,
         )).last
         glyphMapper.SelectColorArray('scalar1')
-        #glyphMapper.SetSourceConnection(0, arrowSource.GetOutputPort())
 
-        cppty = vtkProperty(opacity=0.7, line_width=5, point_size=8, color=(1,0,0))
+        cppty = vtkProperty(line_width=5, point_size=4, color=(0.5,0.5,0.75))
         glyphActor = vtkActor(mapper=glyphMapper, property=cppty)
 
         ren = vtkRenderer()
@@ -83,29 +73,19 @@ class TestCellGridToUnstructuredGrid(Testing.vtkTest):
         rw = vtkRenderWindow()
         rw.AddRenderer(ren)
         ren.SetBackground(1.0, 1.0, 1.0)
-        #ren.SetBackground(0.5, 0.4, 0.3)
-        #ren.SetBackground2(0.7, 0.6, 0.5)
-        #ren.GradientBackgroundOn()
 
-        rwi = rw.MakeRenderWindowInteractor()# vtkRenderWindowInteractor()
+        rwi = rw.MakeRenderWindowInteractor()
         rwi.SetRenderWindow(rw)
 
         cow = vtkCameraOrientationWidget()
         cow.SetParentRenderer(ren)
-        # Enable the widget.
         cow.On()
 
         sid.GetOutputDataObject(0).GetBounds(bds)
         cam = ren.GetActiveCamera()
-        # If you want to change the camera location, this is a good starting point:
-        # dc = ((bds[0]+bds[1])/2, (bds[2]+bds[3])/2, (bds[4] + bds[5])/2)
-        # cam.SetFocalPoint(dc[0], dc[1], dc[2] + 1)
-        # cam.SetPosition(dc[0]+1, dc[1]+1, dc[2]-1.)
-
-        cam.SetViewUp(0., 0., -1.)
-        cam.SetPosition(1.21346, 1.24257, 5.66879)
-        cam.SetFocalPoint(0.146239, 0.394899, 0.0458881)
-        cam.SetDistance(4.63531)
+        rw.Render()
+        ren.ResetCamera()
+        cam.Zoom(2.0)
 
         if '-I' in sys.argv:
             wri = io.vtkCellGridWriter()
@@ -124,8 +104,9 @@ class TestCellGridToUnstructuredGrid(Testing.vtkTest):
         if '-I' in sys.argv:
             rwi.Start()
             print('camera', cam)
-        baseline = 'TestCellGridToUnstructuredGrid.png'
+        baseline = 'TestCellGridToUnstructuredGrid-Centers.png'
         Testing.compareImage(rw, Testing.getAbsImagePath(baseline), threshold=25)
+
     def testMultipleCellTypes(self):
         bds = [0, 0, 0, 0, 0, 0]
 
@@ -183,24 +164,16 @@ class TestCellGridToUnstructuredGrid(Testing.vtkTest):
         rw = vtkRenderWindow()
         rw.AddRenderer(ren)
         ren.SetBackground(1.0, 1.0, 1.0)
-        #ren.SetBackground(0.5, 0.4, 0.3)
-        #ren.SetBackground2(0.7, 0.6, 0.5)
-        #ren.GradientBackgroundOn()
 
-        rwi = rw.MakeRenderWindowInteractor()# vtkRenderWindowInteractor()
+        rwi = rw.MakeRenderWindowInteractor()
         rwi.SetRenderWindow(rw)
 
         cow = vtkCameraOrientationWidget()
         cow.SetParentRenderer(ren)
-        # Enable the widget.
         cow.On()
 
         sid.GetOutputDataObject(0).GetBounds(bds)
         cam = ren.GetActiveCamera()
-        # If you want to change the camera location, this is a good starting point:
-        # dc = ((bds[0]+bds[1])/2, (bds[2]+bds[3])/2, (bds[4] + bds[5])/2)
-        # cam.SetFocalPoint(dc[0], dc[1], dc[2] + 1)
-        # cam.SetPosition(dc[0]+1, dc[1]+1, dc[2]-1.)
 
         cam.SetViewUp(0., 0., -1.)
         cam.SetPosition(1.21346, 1.24257, 5.66879)
@@ -224,7 +197,7 @@ class TestCellGridToUnstructuredGrid(Testing.vtkTest):
         if '-I' in sys.argv:
             rwi.Start()
             print('camera', cam)
-        baseline = 'TestCellGridToUnstructuredGrid.png'
+        baseline = 'TestCellGridToUnstructuredGrid-Mixed.png'
         Testing.compareImage(rw, Testing.getAbsImagePath(baseline), threshold=25)
 
 if __name__ == "__main__":
